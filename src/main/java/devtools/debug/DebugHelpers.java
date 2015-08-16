a7aefc2e6b0236b4dd271bff886c7423841f411f
N. Justus <nicola.justus@mni.thm.de>
2015-08-16 20:31:56 +0200

    Updated javadoc.

--- src/main/java/devtools/debug/DebugHelpers.java
+++ src/main/java/devtools/debug/DebugHelpers.java
@@ -5,38 +5,104 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
 /**
- * Created by nico on 15.08.15.
+ * Helper for debugging a class and for avoiding common problems with null-references, not initialzed values, non-final values etc.
+ * <br/><br/>Created by nico on 15.08.15.
  */
 public final class DebugHelpers {
     private static final String WARNING = "[ WARNING ]: ";
     private DebugHelpers() {}
 
-    public static void  require(boolean b)  {
-        require(b, "Given requirement wasn't fulfilled.");
+    /**
+     * Tests if the given condition is fulfilled.
+     *
+     * @see DebugHelpers#require(boolean, String)
+     * @param condition the tested condition
+     * @throws IllegalArgumentException if the condition isn't fulfilled
+     */
+    public static void  require(boolean condition)  {
+        require(condition, "Given requirement wasn't fulfilled.");
     }
-    public static void require(boolean b, String msg)  {
-        if(!b) throw new IllegalArgumentException(msg);
+
+    /**
+     * Tests if the given condition is fulfilled, throws an exception if not.
+     *
+     * @param condition the tested condition
+     * @param msg the message that should be in the exception
+     * @throws IllegalArgumentException if the condition isn't fulfilled
+     */
+    public static void require(boolean condition, String msg)  {
+        if(!condition) throw new IllegalArgumentException(msg);
     }
+
+    /**
+     * Tests that all given objects aren't null, throws an exception if not.
+     *
+     * @param objects the objects that should be checked against null
+     * @throws NullPointerException if 1 of the objects is null
+     */
     public static void requireNotNull(Object... objects) {
         for(Object o : objects)
             if(o == null)
                 throw new NullPointerException("A non-null reference was required.");
     }
+
+    /**
+     * Tests the given object against null.
+     *
+     * @param o the object for the test
+     * @return true if o != null, false otherwise
+     */
     public static boolean notNull(Object o) {
         return o != null;
     }
+
+    /**
+     * Tests the given object against null, throws an exception if o is null.
+     *
+     * @param o the object for the test
+     * @throws IllegalStateException if o == null
+     */
     public static void notNullError(Object o) {
         if(o == null) throw new IllegalStateException("Given Object ("+o+") was null!");
     }
 
+    /**
+     * Throws an MissingImplementationException() to indicate that an implementation is missing.<br/>
+     * This can be useful when implementing an interface method-for-method.
+     * All not-implemented methods of the interface calls this function and throws an exception,
+     * so all callers of the function knows that this method is missing.
+     *
+     * @throws MissingImplementationException everytime
+     */
     public static void todo() {
         throw new MissingImplementationException();
     }
 
+    /**
+     * Throws an MissingImplementationException() with the given message to indicate that an implementation is missing.<br/>
+     * This can be useful when implementing an interface method-for-method.
+     * All not-implemented methods of the interface calls this function and throws an exception,
+     * so all callers of the function knows that this method is missing.
+     *
+     * @param msg the message for the exception
+     * @throws MissingImplementationException everytime
+     */
     public static void todo(String msg) {
         throw new MissingImplementationException(msg);
     }
 
+    /**
+     * Tests that <B>all fields</B> of the given object <B>aren't null.</B><br/>
+     *
+     * This method is useful when debugging a not-running systems or to check
+     * that every field of the object is initialized with "good" references. <br/><br/>
+     *
+     * Normally this method is called with <B>this</B> as argument.
+     *
+     * @see DebugHelpers#fieldsAreFinal(Object)
+     * @see DebugHelpers#fieldsArePrivate(Object)
+     * @param othis the object with the fields
+     */
     public static void fieldsNotNull(Object othis) {
         final Class<?> clazz = othis.getClass();
         final Field[] fields = clazz.getDeclaredFields();
@@ -52,6 +118,18 @@ public final class DebugHelpers {
         }
     }
 
+    /**
+     * Tests that <B>all fields</B> of the given object <B>are final.</B><br/>
+     *
+     * This method is useful when debugging a not-running systems or to check
+     * that every field of the object is final. <br/><br/>
+     *
+     * Normally this method is called with <B>this</B> as argument.
+     *
+     * @see DebugHelpers#fieldsArePrivate(Object)
+     * @see DebugHelpers#fieldsNotNull(Object)
+     * @param othis the object with the fields
+     */
     public static void fieldsAreFinal(Object othis) {
         final Class<?> clazz = othis.getClass();
         final Field[] fields = clazz.getDeclaredFields();
@@ -66,6 +144,18 @@ public final class DebugHelpers {
         }
     }
 
+    /**
+     * Tests that <B>all fields</B> of the given object <B>are private.</B><br/>
+     *
+     * This method is useful when debugging a not-running systems or to check
+     * that every field of the object is private. <br/><br/>
+     *
+     * Normally this method is called with <B>this</B> as argument.
+     *
+     * @see DebugHelpers#fieldsAreFinal(Object)
+     * @see DebugHelpers#fieldsNotNull(Object)
+     * @param othis the object with the fields
+     */
     public static void fieldsArePrivate(Object othis) {
         final Class<?> clazz = othis.getClass();
         final Field[] fields = clazz.getDeclaredFields();
--- src/main/java/devtools/debug/NullHelpers.java
+++ src/main/java/devtools/debug/NullHelpers.java
@@ -4,21 +4,49 @@ import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-/**
- * Created by nico on 16.08.15.
+/** Contains methods for handling null-pointers.
+ *
+ * <br/><br/>Created by nico on 16.08.15.
  */
 public final class NullHelpers {
     private NullHelpers() { }
 
+    /**
+     * Tests if the given objects aren't null.
+     *
+     *  @see DebugHelpers#requireNotNull(Object...)
+     */
     public static void requireNotNull(Object... objects) {
         DebugHelpers.requireNotNull(objects);
     }
+
+    /**
+     * Tests if the given object isn't null.
+     *
+     * @see DebugHelpers#notNull(Object)
+     */
     public static boolean notNull(Object o) {
         return DebugHelpers.notNull(o);
     }
+
+    /**
+     * Tests if the given object is null and throws an error, if it's null.
+     *
+     * @see DebugHelpers#notNullError(Object)
+     */
     public static void notNullError(Object o) {
         DebugHelpers.notNullError(o);
     }
+
+    /**
+     * Turns the result of the given function into an optional for handling eventually given null-references.
+     *
+     * <br/>This can be handy if a function returns eventually a null-reference.
+     *
+     * @param f the supplier that could return a null-reference
+     * @param <A> the type of the result from f
+     * @return an optional containing the result of f if it's not null, an empty optional otherwise
+     */
     public static <A> Optional<A> nullToOptional(Supplier<A> f) {
         A value = f.get();
         return (value == null) ? Optional.<A>empty() : Optional.of(value);
