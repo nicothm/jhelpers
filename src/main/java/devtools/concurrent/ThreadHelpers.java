727edcbfd6b640f7ad17c15f200f9142f2250f2b
N. Justus <nicola.justus@mni.thm.de>
2015-08-16 20:47:18 +0200

    Updated javadoc.

--- src/main/java/devtools/concurrent/ThreadHelpers.java
+++ src/main/java/devtools/concurrent/ThreadHelpers.java
@@ -2,6 +2,7 @@ package devtools.concurrent;
 
 /**
  * Helpers for working with concurrency, threads, runnables & callables.
+ *
  * <br/><br/>Created by nico on 15.08.15.
  */
 public final class ThreadHelpers {
@@ -9,6 +10,7 @@ public final class ThreadHelpers {
 
     /**
      * Converts the given runnable into a thread.
+     *
      * @param r the runnable for the thread
      * @return a thread that contains r
      */
@@ -21,6 +23,7 @@ public final class ThreadHelpers {
      * except that it explicitly prints-out a stacktrace and exits the program, if an exception is thrown. <br/>
      *
      * This can be useful when working with threadpools, which normally hides exceptions.
+     *
      * @param r the runnable that could throw an exception
      * @return a new runnable which prints out the stacktrace of the thrown exception
      */
--- src/main/java/devtools/math/MathHelper.java
+++ src/main/java/devtools/math/MathHelper.java
@@ -6,13 +6,22 @@ import java.util.Random;
 import static devtools.debug.DebugHelpers.*;
 
 /**
- * Created by nico on 16.08.15.
+ * Helper for common mathematical problems.
+ *
+ * <br/><br/>Created by nico on 16.08.15.
  */
 public final class MathHelper {
     private static Random rnd = new Random();
 
     private MathHelper() { }
 
+    /**
+     * Creates a new random number between the given range.
+     *
+     * @param min the start-value of the range, inclusive
+     * @param max the end-value of the range, exclusive
+     * @return a random-number between min and max
+     */
     public static int getRandom(final int min, final int max) {
         require(max>0 && min>0, "Min & Max must be >0.");
         require(max > min, "Max has to be  > min.");
@@ -20,14 +29,36 @@ public final class MathHelper {
         return rnd.nextInt( (max-min) ) + min;
     }
 
+    /**
+     * Rounds the given double-value onto the given scale.
+     *
+     * @param no the number for the rounding-operation
+     * @param scale the number of decimals
+     * @return no rounded on scale's decimals
+     */
     public static double round(double no, int scale) {
         BigDecimal bd = new BigDecimal(no).setScale(scale, BigDecimal.ROUND_HALF_UP);
         return bd.doubleValue();
     }
 
+    /**
+     * Rounds the given number on 2 decimals.
+     *
+     * @see MathHelper#round(double, int)
+     * @param no the number for the rounding-operation
+     * @return no rounded on 2 decimals
+     */
     public static double roundOnTwo(double no) {
         return round(no, 2);
     }
+
+    /**
+     * Rounds the given number on 3 decimals.
+     *
+     * @see MathHelper#round(double, int)
+     * @param no the number for the rounding-operation
+     * @return no rounded on 3 decimals
+     */
     public static double roundOnThree(double no) {
         return round(no, 3);
     }
